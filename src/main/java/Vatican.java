96dce893ecef5a16d5676af63ce1f37a3bcbdc05
lamemario <146546211+lamemario@users.noreply.github.com>
2026-01-23 19:14:59 +0800

    Level-0: Implement skeletal version of Vatican
    Rename Duke to Vatican and implement a basic greeting and exit
    sequence.
    
    Changes:
    - Renamed Duke.java to Vatican.java
    - Added Vatican ASCII logo
    - Updated README.md to remove Duke-related elements

---
+++ src/main/java/Vatican.java
@@ -0,0 +1,23 @@
+public class Vatican {
+    public static void main(String[] args) {
+        String logo = """
+                 __      __   _   _                \s
+                 \\ \\    / /  | | (_)               \s
+                  \\ \\  / /_ _| |_ _  ___ __ _ _ __ \s
+                   \\ \\/ / _` | __| |/ __/ _` | '_ \\\s
+                    \\  / (_| | |_| | (_| (_| | | | |
+                     \\/ \\__,_|\\__|_|\\___\\__,_|_| |_|
+            """;
+
+        String horizontalLine = "____________________________________________________________";
+
+        System.out.println(horizontalLine);
+        System.out.println(logo);
+        System.out.println(" Hello! I'm Vatican");
+        System.out.println(" What can I do for you?");
+        System.out.println(horizontalLine);
+
+        System.out.println(" Bye. Hope to see you again soon!");
+        System.out.println(horizontalLine);
+    }
+}
