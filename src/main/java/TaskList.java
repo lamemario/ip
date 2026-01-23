07ea031e5fd9a914a5ed3ebff9c0315d89457015
lamemario <146546211+lamemario@users.noreply.github.com>
2026-01-24 02:36:50 +0800

    Complete implementation of TaskList and Ui

--- src/main/java/TaskList.java
+++ src/main/java/TaskList.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
+public class TaskList {
+    private Task[] tasks = new Task[100];
+    private int taskCount = 0;
 
-public class taskList {
+    public void addTask(String description) {
+        tasks[taskCount++] = new Task(description);
+    }
+
+    public void markTask(int index) {
+        tasks[index].markAsDone();
+    }
+
+    public void unmarkTask(int index) {
+        tasks[index].unmarkAsDone();
+    }
+
+    public Task getTask(int index) {
+        return tasks[index];
+    }
+
+    public int getSize() {
+        return taskCount;
+    }
 }
--- src/main/java/Ui.java
+++ src/main/java/Ui.java
@@ -1,4 +1,24 @@
-package PACKAGE_NAME;
-
 public class Ui {
+    private static final String LINE = "____________________________________________________________";
+
+    public void showLine() {
+        System.out.println(LINE);
+    }
+
+    public void showWelcome(String logo) {
+        showLine();
+        System.out.println(logo);
+        System.out.println(" More life. It's Vatican, dun know.");
+        System.out.println(" So, what's the deal? I'm tryna see how I can bless you.");
+        showLine();
+    }
+
+    public void showTaskList(TaskList tasks) {
+        showLine();
+        System.out.println(" Check the list out, styll:");
+        for (int i = 0; i < tasks.getSize(); i++) {
+            System.out.println(" " + (i + 1) + "." + tasks.getTask(i));
+        }
+        showLine();
+    }
 }
