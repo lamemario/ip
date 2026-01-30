0956950591754d0941896cc4eb42a399e325378f
lamemario <146546211+lamemario@users.noreply.github.com>
2026-01-30 17:16:52 +0800

    A-MoreOOP: Implement Parser with basic ExitCommand support

---
+++ src/main/java/Parser.java
@@ -0,0 +1,13 @@
+public class Parser {
+    public static Command parse(String fullCommand) throws VaticanException {
+        String[] parts = fullCommand.split(" ", 2);
+        CommandType type = CommandType.fromString(parts[0]);
+
+        switch (type) {
+        case BYE:
+            return new ExitCommand();
+        default:
+            throw new VaticanException("I haven't learned that move in the new system yet, styll.");
+        }
+    }
+}
